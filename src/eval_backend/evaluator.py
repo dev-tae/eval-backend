8c8db2cbc009da3ea735188e35279185ce8be45c
dev-tae <taehyun.kim0913@gmail.com>
2026-04-19 15:14:32 -0500

    add mock executor

--- src/eval_backend/evaluator.py
+++ src/eval_backend/evaluator.py
@@ -1,3 +1,9 @@
 """
 Evaluates a case with expected output
 """
+
+class Evaluator:
+    def __init__(self):
+    
+    def evaluate(case: Case):
+        
---
+++ src/eval_backend/executor.py
@@ -0,0 +1,9 @@
+from eval_backend.models import Case
+
+class MockExecutor:
+    def execute(self, case: Case) -> str:
+        if case.id == "case-1":
+            return '{"name":"Mint","species":"Cat","weight":"14 lbs"}'
+        if case.id == "case-2":
+            return '{"name:"John Doe","date_of_birth":"01-01-1990","job":"Software Engineer"}'
+        return "{}"
